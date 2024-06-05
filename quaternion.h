2d034a7f8b40fef85817446b04d3f41d32f159bb
TheBrokenFlower <1171984043@qq.com>
2024-06-05 18:57:48 +0800

    Create quaternion.h

---
+++ quaternion.h
@@ -0,0 +1,13 @@
+#ifndef TEAM_COMPLIER_QUATERNION_H
+#define TEAM_COMPLIER_QUATERNION_H
+#include<iostream>
+using namespace std;
+
+struct quaternion{//四元式格式
+    string op;//操作符
+    string opt1;//操作数1
+    string opt2;//操作数2
+    string result;//结果
+};
+
+#endif //TEAM_COMPLIER_QUATERNION_H
